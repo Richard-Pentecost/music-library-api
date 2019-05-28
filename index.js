0337e7cd5722aca5ffdada58f187a218d18b6df9
Richard Pentecost <richardpentecost1@gmail.com>
2019-05-28 20:39:06 +0100

    database connection set up

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 const app = require('./src/app');
 
 mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true }, () => {
+  console.log('Database running...');
   app.listen(3000);
 });
