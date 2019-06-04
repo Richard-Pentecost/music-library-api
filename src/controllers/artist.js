e3a6fa0770776ff0751b55f4517a8cc23afa60d5
Richard Pentecost <richardpentecost1@gmail.com>
2019-06-04 17:01:46 +0100

    find artist by ID done using find method but without chaining arguments

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -22,13 +22,11 @@ exports.list = (req, res) => {
 };
 
 exports.find = (req, res) => {
-  Artist.find()
-    .where('_id').equals(req.params.artistId)
-    .exec((err, artist) => {
-      if (!artist) {
-        res.status(404).json({ error: 'The artist could not be found.' });
-      } else {
-        res.status(200).json(artist[0]);
-      }
-    });
+  Artist.find({ _id: req.params.artistId }, (err, artist) => {
+    if (!artist) {
+      res.status(404).json({ error: 'The artist could not be found.' });
+    } else {
+      res.status(200).json(artist[0]);
+    }
+  });
 };
